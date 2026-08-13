fbc2af0e1dc22195af74b7b4d83f160ba5e00c5a
~• 𝖘𝖔𝖋𝖎𝖆 •~ <paulo.americano@escola.pr.gov.br>
2026-08-13 07:46:49 -0300

    m

---
+++ catalogo.js
@@ -0,0 +1,16 @@
+export default class catalogo {
+    #listademidias;
+    constructor() {
+        this.#listademidias = {};
+    }
+    adicionar(midia) {
+        this.#listademidias.push(midia);
+        this.renderizar();
+        ort default class catalogo {
+        }
+            renderizar() {
+                const container
+            }
+        
+    }
+}
